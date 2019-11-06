99747e38352bfcc4c042b732b6e5910b99ebf9a1
Belie06Loryn <maniralexie@gmail.com>
2019-11-06 12:06:48 +0200

    navbar

--- bread/views.py
+++ bread/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def page(request):
+    return render(request,'all-pages/index.html',{})
+
